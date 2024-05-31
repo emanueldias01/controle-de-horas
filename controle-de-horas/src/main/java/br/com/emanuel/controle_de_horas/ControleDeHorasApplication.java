2daa0b5a246c589dbace8c910e1597a9a7f7aa75
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-05-31 08:10:24 -0300

    adicao dos arquivos

---
+++ controle-de-horas/src/main/java/br/com/emanuel/controle_de_horas/ControleDeHorasApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanuel.controle_de_horas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeHorasApplication.class, args);
+	}
+
+}
---
+++ controle-de-horas/src/test/java/br/com/emanuel/controle_de_horas/ControleDeHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanuel.controle_de_horas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
